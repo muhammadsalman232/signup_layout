88b4597a82a662eafeb8397cfb7c60bba4f482f5
muhammadsalman232 <muhammadsalman2826@gmail.com>
2025-12-18 13:11:06 +0500

    Added ImageView img_light_dot to FourthActivity

--- app/src/main/java/com/example/signup_layout/FourthActivity.java
+++ app/src/main/java/com/example/signup_layout/FourthActivity.java
@@ -3,18 +3,23 @@ package com.example.signup_layout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 public class FourthActivity extends AppCompatActivity {
     AppCompatButton next_btn;
+    ImageView img_light_dot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fourth_activity);
         next_btn = findViewById(R.id.next_btn);
+        img_light_dot = findViewById(R.id.img_light_dot);
         next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
